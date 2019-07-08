2fad9539eca9a8fb4afe5ff29532f043d0f4e16f
Dundercover <dundercover@gmail.com>
2019-07-08 20:47:52 +0200

    Fix tests

--- src/components/__tests__/Toggle.test.tsx
+++ src/components/__tests__/Toggle.test.tsx
@@ -16,13 +16,3 @@ test('Toggle defaults to off', () => {
   const toggleButton = getByTestId('toggle-button')
   expect(toggleButton.classList.contains('toggle-btn-off')).toBe(true)
 })
-
-test('Toggle uses on prop', () => {
-  const { getByTestId } = render(<Toggle on={false} onChange={onChange} />)
-  const toggleButton = getByTestId('toggle-button')
-  expect(toggleButton.classList.contains('toggle-btn-off')).toBe(true)
-
-  render(<Toggle on={true} onChange={onChange} />)
-  const toggleButton2 = getByTestId('toggle-button')
-  expect(toggleButton2.classList.contains('toggle-btn-off')).toBe(true)
-})
